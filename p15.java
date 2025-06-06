d7f67f39fa15c406f1bd0d51ac93e277fcbc5c06
kManikant <121446582+kManikant@users.noreply.github.com>
2025-06-06 13:28:26 +0530

    final pattern (HARD)

--- p15.java
+++ p15.java
@@ -6,16 +6,26 @@ public class p15 {
         int n = sc.nextInt();
         int star=1;
         int row=1;
+        int val =1;
         int space=n-1;
         while (row<=2*n-1) {
+            //space
           int i=1;
           while(i<=space){
             System.out.print("  ");
             i++;
           }
+          //star
           int j =1;
+          int p=val;
           while(j<=star){
-            System.out.print("* ");
+            System.out.print(p + " ");
+            if(j<=star/2){
+                   p++;
+            }else{
+                p--;
+            }
+           
             j++;
           }
           //next row prep
@@ -23,10 +33,13 @@ public class p15 {
           if(row<n){
             star+=2;
             space--;
+            val++;
           }else{
             star-=2;
             space++;
+            val--;
           }row++;
+          
           System.out.println();
             
         }
