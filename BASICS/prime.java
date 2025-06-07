a2735a2de48d02c8cd9be7f496afb5de74c5b2de
kManikant <121446582+kManikant@users.noreply.github.com>
2025-06-07 11:17:26 +0530

    prime number

---
+++ BASICS/prime.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class prime{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+       
+        int count =0;
+         int i=2;
+        while(i<n-1){
+            if(n%i==0){
+                count++;
+            }i++;
+        }
+        if(count>=1){
+            System.out.println("not prime");
+        }else{
+            System.out.println("prime");
+        }
+    }
+}
