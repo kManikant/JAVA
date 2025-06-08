64b5b59889d33dd16f38b0ce98f823db173008dc
kManikant <121446582+kManikant@users.noreply.github.com>
2025-06-08 15:26:28 +0530

    dec/bin to bin/dec

--- BASICS/decimal_to_binary.java
+++ BASICS/decimal_to_binary.java
@@ -2,12 +2,14 @@ public class decimal_to_binary {
     public static void main(String[] args) {
         int a=9;//base 10
         int sum=0;
-        int mul=1;
+        int mul=1;         
+                                
         while(a>0){
-             int rem =a%2;//base 2
+             int rem =a%2;//base 2         //if binary to decimal   int rem =a%10;
              sum = sum + rem *mul;
-             mul = mul*10;
-             a=a/2;
+             mul = mul*10;                // mul =mul*2
+             a=a/2;                      // a=a/10
         }System.out.println(sum);
     }
 }
+ 
