2e738de9fc3e6f2ed517de60cee056adf7863388
kManikant <121446582+kManikant@users.noreply.github.com>
2025-06-10 21:29:58 +0530

    max

--- Arrays/max2.java
+++ Arrays/max2.java
@@ -1,4 +1,5 @@
 public class max2 {
+    
     public static void main(String[] args) {
         int [] arr ={22,33,4,5,60,678};
     System.out.println(maximum(arr));
@@ -6,7 +7,7 @@ public class max2 {
 
     public static int maximum(int[] arr){
         int max= Integer.MIN_VALUE;// -2^31
-        //Integer.MAX_VALUE 2^31-1
+                                  //Integer.MAX_VALUE 2^31-1
         int i=0;
         while (i<arr.length) {
             if(arr[i]>max){
