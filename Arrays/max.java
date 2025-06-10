90891fae51a5b4155dc120efa88b309176c5141d
kManikant <121446582+kManikant@users.noreply.github.com>
2025-06-10 19:26:07 +0530

    max value

---
+++ Arrays/max.java
@@ -0,0 +1,17 @@
+public class max {
+    public static void main(String[] args) {
+        int [] arr ={22,33,4,5,6};
+    System.out.println(maximum(arr));
+    }
+
+    public static int maximum(int[] arr){
+        int max= arr[0];
+        int i=1;
+        while (i<arr.length) {
+            if(arr[i]>max){
+                max=arr[i];
+                      }i++;
+        }
+      return max;
+    }
+}
