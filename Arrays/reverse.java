43f3cea277d01ea13dce52a8aec904d9a0d957f0
kManikant <121446582+kManikant@users.noreply.github.com>
2025-06-11 14:22:47 +0530

    reverse

--- Arrays/reverse.java
+++ Arrays/reverse.java
@@ -1,12 +1,12 @@
 public class reverse {
     public static void main(String[] args) {
         int[] arr ={3,4,5,6,7};
-        reverse(arr);
+        reversed(arr);
         for(int i=0;i<arr.length;i++){
               System.out.print(arr[i]+" ");
         }
     }
-    public static void reverse(int[] arr){
+    public static void reversed(int[] arr){
           int i=0;
           int j =arr.length-1;
           while(i<j){
