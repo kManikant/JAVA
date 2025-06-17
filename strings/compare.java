fac29054271e023be4483d55a4ee6c9278ceb1e4
kManikant <121446582+kManikant@users.noreply.github.com>
2025-06-17 17:49:49 +0530

    lexico_order

---
+++ strings/compare.java
@@ -0,0 +1,21 @@
+public class compare {
+    public static void main(String[] args) {
+        String s="owwbe";
+        String s1=new String("wwbe");
+        String s2=new String("wwe");
+        System.out.println(compare(s1,s2));
+    }
+            // comare in lixical/dictionary order 
+    public static int compare(String s1,String s2){
+        if (s1==s2) {  // for  string pool condition
+            return 0; // 0 vlue dentoes s1 = s2 
+        }
+        for(int i=0;i<Math.min(s1.length(),s2.length());i++){
+            if(s1.charAt(i)!=s2.charAt(i)){
+                return s1.charAt(i)-s2.charAt(i); // -ve value denotes s1<s2 , 
+                                                  // +ve value denotes s1>s2
+            }
+        }
+     return s1.length()-s2.length();
+    }
+}
